94f18960ff2e0b50f2001c2bbc9a178b02f159c7
Bugsy <32585986+p1pk4@users.noreply.github.com>
2021-09-14 16:36:51 +0300

    some tests in tests.py

---
+++ tests.py
@@ -0,0 +1,46 @@
+# total = 0
+# for i in range(1, 6):
+#     total += i
+#     print(total, end='')
+
+
+# for index, element in enumerate(my_list):
+#     print(index, element)
+
+# print(my_list)
+# for index in enumerate(my_list):
+
+#         # my_list2[::] = my_list1
+# # print(my_list2)
+
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+# my_list1 = [1, 3, 5, 7, 9, 11]
+# my_list2 = [2, 4, 6, 8, 10, 12]
+
+# my_list[::2] = my_list2
+# my_list[1::2] = my_list1
+# print(my_list)
+
+# num = int(input())
+
+# my_list = []
+# new_list = []
+# for i in range(num):
+#     num = int(input())
+#     i += 1
+#     my_list.append(num)
+#     new_list.append(num)
+#     # for index, value in enumerate(my_list):
+#     #     print(index, value)
+# my_list.sort()
+# new_list.sort()
+# maxnum = max(my_list)
+# new_list.pop(-1)
+# mnum = max(new_list)
+# print(maxnum)
+# print(mnum)
+
+my_tuple1 = (1,)
+my_tuple2 = (2,)
+my_tuple3 = (3,)
+print(type(my_tuple1))
