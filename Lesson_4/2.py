74fbb42742165fff137dd49a1f190da0dec98447
ToxicBugsy <pipkagr@gmail.com>
2021-09-19 18:23:41 +0300

    Task 2 done(1/2). Task 4 uncompleated

--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -14,27 +14,13 @@ Task 2.
 
 a_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 
-out = [i for i in range(20, 241, 20)]
 
-print(next(out))
-
-# print(type(my_list))
+out = [item for item in a_list]
+print(out)
 
 
-
-# # Создаем список
-# alist = [4, 16, 64, 256]
-
-# # Вычислим квадратный корень, используя генерацию списка
-# out = [a**(1/2) for a in alist]
-# print(out)
-# print(type(out))
-# # Используем выражение генератора, чтобы вычислить квадратный корень
-# out = (a**(1/2) for a in alist)
-# print(out)
-# print(next(out))
-# print(next(out))
-# print(next(out))
-# print(next(out))
-# print(type(out))
-# # print(next(out))
+for item in range(1,len(out)):
+    if out[item] > out[item - 1]: #range(1, len(a_list)) if a_list[item] > a_list[item - 1]]
+        
+        
+        print(out[item])
