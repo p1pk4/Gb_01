a0e5830bdc3e5c9f02d53b7ea3dae7dcc34b9b23
ToxicBugsy <pipkagr@gmail.com>
2021-09-22 13:23:31 +0300

    done

--- Lesson_4/Lesson_4_Task_4.py
+++ Lesson_4/Lesson_4_Task_4.py
@@ -14,23 +14,6 @@ Task 4.
 
 
 mlist = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-
-
-
 new_list = [] 
 s = [new_list.append(x) for x in mlist if x not in new_list]
 print(list(new_list)) 
-
-# x = [elem[0] for elem in groupby(mlist)]
-# print(tuple(x))
-
-# # print(list(set(mlist)))
-# b = []
-# a = [int(item) for item in mlist]
-# # print(a)
-
-# for item in mlist:
-#     if mlist[0] != mlist[item]:
-        
-#         b.append(item)
-#         print(list(b))
