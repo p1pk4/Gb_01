724bc9f1d93528698399306e712da20863d31982
ToxicBugsy <pipkagr@gmail.com>
2021-09-24 16:18:01 +0300

    t2 done

--- Lesson_6/Lesson_6_Task_2.py
+++ Lesson_6/Lesson_6_Task_2.py
@@ -13,8 +13,14 @@
 Task 2.
 '''
 
-class Road:
 
+class Road:
+    """\n\033[38;5;45mРасчет массы асфальта.
+
+    Значения длины, ширины, массы и толщины задаются в экземпляре класса.
+    Расчет в методе 'calc_of_mass'. 
+    Формула: __length * __width * __weight * __thickness --> result\033[0;0m
+    """
     def __init__(self, __length, __width, __weight, __thickness):
         self.__length = __length
         self.__width = __width
@@ -26,6 +32,30 @@ class Road:
         print(result)
         return result
         
+print(Road.__doc__)
 # Экземпляр класса с защищенными атрибутами. 
 new_road = Road(5000, 20, 25, 5)
 new_road.calc_of_mass()
+
+
+# Сначала сделал с наследованием и переопределением, потом переделал по условию.
+class SecondRoad:
+    # __length = 0
+    # __width = 0
+    # __weight = 0
+    # __thickness = 0
+    pass
+
+class NewSecondRoad(SecondRoad):
+    __length = 5000
+    __width = 20
+    __weight = 25
+    __thickness = 5
+
+    def calc_of_mass(self):
+        result = new_second_road.__length * new_second_road.__width * new_second_road.__weight * new_second_road.__thickness
+        print(f'\nВторая дорога - {result}')
+        
+# Экземпляр класса с защищенными атрибутами. 
+new_second_road = NewSecondRoad()
+new_second_road.calc_of_mass()
